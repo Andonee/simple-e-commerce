96b0d8af46384b0b7b7c8d1ba8628151f0426d67
mpoprawa <mateusz.poprawa@gmail.com>
2021-09-07 16:35:53 +0200

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import { Route, Switch, HashRouter as Router } from 'react-router-dom'
+
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import { Contact, Home, Products } from './pages'
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<Router>
+			<div className='App'>
+				<Navbar />
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/products/:category' component={Products} />
+					<Route exact path='/contact' component={Contact} />
+				</Switch>
+			</div>
+		</Router>
+	)
 }
 
 export default App
